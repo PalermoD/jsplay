53d12c3c3a0df82a57e6c0c3eb48344697cb9ef1
PalermoD <palermo.deschamps@gmail.com>
2016-01-22 13:27:11 -0500

    added a ui for my object literal to print a random number in my dice

--- scripts.js
+++ scripts.js
@@ -23,8 +23,8 @@ function diceRoll(){
 var dice = {
 	sides : 6,
 	roll : function(){
-	var randomNumber = Math.floor( Math.random() * dice.sides) + 1;
-	console.log(randomNumber);
+	var randomNumber = Math.floor( Math.random() * this.sides) + 1;
+	return randomNumber;
     }
 }
 
---
+++ ui.js
@@ -0,0 +1,12 @@
+var printNumber = function(number){
+    var placeholder = document.getElementById("placeholder");
+    placeholder.innerHTML = number;
+}
+
+var button = document.getElementById("button");
+
+button.onclick = function(){
+	var result = dice.roll();
+	printNumber(result);
+	
+}
